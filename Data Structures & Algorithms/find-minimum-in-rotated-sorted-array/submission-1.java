8951fd9154cddce645a7ff0739e8acd70825b728
sewerratmoney <twon42069@gmail.com>
2026-08-12 18:42:26 -0700

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/find-minimum-in-rotated-sorted-array/submission-1.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findMin(int[] nums) {
+        int low = 0;
+        int high = nums.length - 1;
+        while (low < high) {
+            if (nums[low] < nums[high]) {
+                return nums[low];
+            }
+            int middle = low + (high - low) / 2;
+            if (nums[middle] > nums[high]) {
+                low = middle + 1;
+            } else {
+                high = middle;
+            }
+        }
+        return nums[low];
+    }
+}
